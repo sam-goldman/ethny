dccd0bb25d5f72cbce5250c6231b1ab3751d0a03
lc <corben@lynxsecurity.io>
2022-06-25 10:45:50 -0400

    feat(ethny): add royalties

--- hardhat/contracts/Grid.sol
+++ hardhat/contracts/Grid.sol
@@ -2,17 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-contract Grid is ERC721, ReentrancyGuard, Ownable {
+contract Grid is ERC721, IERC2981, ReentrancyGuard, Ownable {
     // public constant TOTAL_SUPPLY = ;
 
     mapping(uint256 => uint8) _tokenIds;
+    uint256 public royaltiesPercentage = 10;
 
     // mapping: tokenId => current price
 
-    constructor(uint256 _maxSupply) ERC721("Grid", "GRD") {}
+    constructor(uint256 _maxSupply) ERC721("Grid", "GRD") {
+    }
 
     // include events on minting and transfers
 
@@ -33,14 +36,27 @@ contract Grid is ERC721, ReentrancyGuard, Ownable {
             _safeMint(_msgSender(), _mintAmount);
         }
     }
-
-    // royalties
-    // -royaltyinfo + setroyaltyies + supportsinterface (erc-2981)
-
     // getter functions for mapping(s)
 
     // token uri should return rgb
 
+
+    // royalties
+    // -royaltyinfo + setroyaltyies + supportsinterface (erc-2981)
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, IERC2981) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    // set a new royalty percentage
+    function setRoyaltiesPercentage(uint256 newPercentage) public onlyOwner {
+        royaltiesPercentage = newPercentage;
+    }
+
+    function royaltyInfo(uint256 tokenId, uint256 _salePrice) external view override(IERC2981) returns(address receiver, uint256 royaltyAmount) {
+        uint256 _royalties = ((_salePrice * royaltiesPercentage) / 100);
+        return (owner(), _royalties);
+    }
+    
     // withdraw function
     function withdraw() public onlyOwner {
         require(address(this).balance > 0, "Balance is 0");
